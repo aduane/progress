dd197b93598d1f324a1f777c278ff5aa739d26cb
Andrew Duane <andrew.m.duane@gmail.com>
2017-05-19 22:15:10 -0700

    Make progress bar helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@
 
 # Main helper file
 module ApplicationHelper
+  def progress_bar(label:, numerator:, denominator:, units:)
+    percent = ((numerator.to_f / denominator.to_f) * 100).round(1)
+    content_tag(:div) do
+      content_tag(:span, label) +
+        content_tag(:progress, nil, class: 'bar', max: denominator,
+                                    value: numerator) +
+        content_tag(:span,
+                    "#{numerator} / #{denominator} #{units} – #{percent}% " \
+                    'complete')
+    end
+  end
 end
